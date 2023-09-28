ad8466ef2d71365230876c1e67c2ef17a72097bd
= <aymancoom3@gmail.com>
2023-09-28 14:53:31 +0300

    Ghool

--- mainFile.py
+++ mainFile.py
@@ -2,14 +2,9 @@ from connectionExecution import fillResults
 
 headerFooterResult , rowResult = fillResults(50)
 
-# print(headerFooterResult)
-# print("------------------------------------")
-# print(rowResult)
-
 
 for hfRow in headerFooterResult:
     print(hfRow) # Prints the Tuple Of the Data 
-
 
 print("----------------------------") # Seperation Of Rows From Headers and Footer
 for rRow in rowResult:
