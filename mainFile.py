904c43ed03c23a552ede076b4f3f65c30319ff7c
= <aymancoom3@gmail.com>
2023-09-28 14:46:34 +0300

    Seperation and Loop For th eData

--- mainFile.py
+++ mainFile.py
@@ -2,6 +2,16 @@ from connectionExecution import fillResults
 
 headerFooterResult , rowResult = fillResults(50)
 
-print(headerFooterResult)
-print("------------------------------------")
-print(rowResult)
+# print(headerFooterResult)
+# print("------------------------------------")
+# print(rowResult)
+
+
+for hfRow in headerFooterResult:
+    print(hfRow) # Prints the Tuple Of the Data 
+
+
+print("----------------------------") # Seperation Of Rows From Headers and Footer
+for rRow in rowResult:
+    print(rRow) ## Print Each Tuple Of Items In FATOORA 
+    print("****************")
