8a2fb8442768692f3abe7f27c3f9c9877a76d4cd
= <aymancoom3@gmail.com>
2023-09-28 14:27:06 +0300

    Text Replacement and Data Are Ok

--- mainFile.py
+++ mainFile.py
@@ -1,4 +1,27 @@
-# The Algorithm 
+import pyodbc
+from functions import replaceHeaderFooterQuery , replaceRowsQuery
+
+conn = pyodbc.connect("Driver={SQL Server};"
+                "Server=10.10.10.100;"
+                "Database=LB;"
+                "UID=ayman;"
+                "PWD=admin@1234;")
+
+headerfooterQuery = replaceHeaderFooterQuery(str(50))
+rowQuery  = replaceRowsQuery(str(50))
+
+
+cursor = conn.cursor()
+cursor.execute(headerfooterQuery)
+headerFooterResult  = cursor.fetchall()
+cursor.execute(rowQuery)
+rowResult  = cursor.fetchall()
+
+print(headerFooterResult)
+print("------------------------------------")
+print(rowResult)
+
+
 # Make Two Functions To Get the DocNum and Return the Query Concatenated With it 
 # Make Functions To Run The Queries and Return the Results 
 # Get Results and Parse Them 
