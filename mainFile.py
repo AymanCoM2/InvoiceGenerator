f0220fbb5ddd148b063a86d1ec158fe2811ae85a
= <aymancoom3@gmail.com>
2023-09-28 14:20:34 +0300

    Functions Seperated

---
+++ mainFile.py
@@ -0,0 +1,7 @@
+# The Algorithm 
+# Make Two Functions To Get the DocNum and Return the Query Concatenated With it 
+# Make Functions To Run The Queries and Return the Results 
+# Get Results and Parse Them 
+# Loop and FIll the Tables and the PlaceHolders 
+
+# 
