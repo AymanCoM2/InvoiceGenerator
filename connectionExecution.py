80976fbb712eb1b45a22fdcd27adad6d1a0ac5f2
= <aymancoom3@gmail.com>
2023-09-30 12:55:28 +0300

    New Template

--- connectionExecution.py
+++ connectionExecution.py
@@ -1,11 +1,11 @@
 import pyodbc
 from functions import replaceHeaderFooterQuery , replaceRowsQuery
-def fillResults(docNumber):
+def connectionResults(docNumber):
     headerfooterQuery = replaceHeaderFooterQuery(str(docNumber))
     rowQuery  = replaceRowsQuery(str(docNumber))
     conn = pyodbc.connect("Driver={SQL Server};"
                     "Server=10.10.10.100;"
-                    "Database=LB;"
+                    "Database=TM;"
                     "UID=ayman;"
                     "PWD=admin@1234;")
     cursor = conn.cursor()
@@ -14,3 +14,4 @@ def fillResults(docNumber):
     cursor.execute(rowQuery)
     rowResult  = cursor.fetchall()
     return headerFooterResult , rowResult
+
--- functions.py
+++ functions.py
@@ -8,11 +8,9 @@ def replaceHeaderFooterQuery(replacement):
         T0.DiscPrcnt , T0.DiscSum ,
         (T0.DocTotal - T0.RoundDif - T0.VatSum) 'NetTotalBefVAT',
         T0.VatSum , T0.DocTotal , T00.U_NAME , T0.Comments 
-
         FROM TM.DBO.OINV T0
         LEFT JOIN TM.DBO.NNM1 N1 ON N1.Series = T0.Series
         LEFT JOIN  TM.DBO.OUSR T00 ON T0.USERSIGN = T00.INTERNAL_K
-
         WHERE 
         T0.CANCELED ='N' and T0.DocEntry = 'X' 
         """
@@ -26,11 +24,10 @@ def replaceRowsQuery(replacement):
         ROUND(T1.Price * T1.Quantity,2) 'TotalBefVAT',
         ROUND(ROUND(T1.Price * T1.Quantity,2) * 1.05 ,2) 'TotalAftVAT'
         FROM 
-        (LB.DBO.OINV T0 inner join LB.DBO.INV1 T1 on T1.DocEntry= T0.DocEntry)
-        LEFT JOIN (LB.DBO.OWHS W0 LEFT JOIN LB.DBO.OLCT L0 ON W0.Location = L0.Code)
+        (TM.DBO.OINV T0 inner join TM.DBO.INV1 T1 on T1.DocEntry= T0.DocEntry)
+        LEFT JOIN (TM.DBO.OWHS W0 LEFT JOIN TM.DBO.OLCT L0 ON W0.Location = L0.Code)
         ON W0.WhsCode = T1.WhsCode
-
-        WHERE T1.DocEntry IN (SELECT T0.DocEntry FROM LB.DBO.OINV T0 WHERE T0.CANCELED ='N') and 
+        WHERE T1.DocEntry IN (SELECT T0.DocEntry FROM TM.DBO.OINV T0 WHERE T0.CANCELED ='N') and 
         T1.DocEntry = 'X'
     """
     return rowsQuery.replace('X', replacement)
